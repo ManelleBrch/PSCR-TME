d5cab175915f2f844875d6f678ec60d8e7254464
Manelle Berrichi <manelle.berrichi@etu.sorbonne-universite.fr>
2022-10-28 11:33:35 +0200

    debut tme6 q2 marche

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
+
+
+using namespace std;
 
 int main () {
+	int nbfils = 0;
 	const int N = 3;
 	std::cout << "main pid=" << getpid() << std::endl;
+	pid_t pid;
+	int j = N;
+	for (int i=1; i<=N && j==N ; i++ ) {
+		if((pid =fork()) == 0){
+			nbfils =0;
+			std::cout << " i:j " << i << ":" << j << std::endl;
+			for (int k=1; k<=i && j==N ; k++) {
+				if ((pid =fork()) == 0) {
+					nbfils = 0;
+					j=0;
+					std::cout << " k:j " << k << ":" << j << std::endl;
+				}else{
+					nbfils++;
 
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
-		std::cout << " i:j " << i << ":" << j << std::endl;
-		for (int k=1; k<=i && j==N ; k++) {
-			if ( fork() == 0) {
-				j=0;
-				std::cout << " k:j " << k << ":" << j << std::endl;
+				}
 			}
+		}
+		else{
+			nbfils++;
+			break;
+		}
+	}
+
+	if(j == N){
+		for(int m = 0; m < nbfils; m++){
+			cout << "J'attend mon fils" << endl;
+			wait(nullptr);
 		}
 	}
 	return 0;
